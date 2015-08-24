6a7d53df76029879c26b959a4e6349aa172a3691
vihuhol <ss2015@ws-2k-110-n05.ccam.cmc.unn.net>
2015-08-24 14:56:35 +0300

    add parser

--- src/object_recognition.cpp
+++ src/object_recognition.cpp
@@ -2,9 +2,33 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
+#include <fstream>
+#include <iostream>
 
+using namespace cv;
+using namespace std;
+
+const char* params =
+     "{ h | help          | false | print usage                                   }"
+     "{   | sample-list   | false | path to list with image classes names         }"
+     "{   | image         |       | image to detect objects on                    }"
+     "{   | camera        | false | whether to detect on video stream from camera }";
+
+vector<Mat> samples;
+vector<string> classNames;
+
+void SampleListParser(int argc, const char **argv)
+{
+    CommandLineParser parser(argc, argv, params);
+    string sampleListFile = parser.get<string>("sample-list");
+    ifstream sampleListFileReader(sampleListFile);
+    char buff[50];
+    sampleListFileReader >> buff;
+    cout << buff;
+}
 
 int main(int argc, const char **argv)
 {   
+    SampleListParser(argc, argv);
     return 0;
 }
