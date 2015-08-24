c2adefb2f6deef93b551a126cbd728bf555b4ce3
vihuhol <ss2015@ws-2k-110-n05.ccam.cmc.unn.net>
2015-08-24 15:12:29 +0300

    Parser added

--- src/object_recognition.cpp
+++ src/object_recognition.cpp
@@ -2,23 +2,33 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2\nonfree\features2d.hpp"
+#include <fstream>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
-void FindFeatureUseSURF(const Mat image)
+const char* params =
+     "{ h | help          | false | print usage                                   }"
+     "{   | sample-list   | false | path to list with image classes names         }"
+     "{   | image         |       | image to detect objects on                    }"
+     "{   | camera        | false | whether to detect on video stream from camera }";
+
+vector<Mat> samples;
+vector<string> classNames;
+
+void SampleListParser(int argc, const char **argv)
 {
-	int minHessian = 400;
-
-	SurfFeatureDetector detector( minHessian );
-
-	vector<KeyPoint> keypoints_object;
-
-	detector.detect( image, keypoints_object );
-	
+    CommandLineParser parser(argc, argv, params);
+    string sampleListFile = parser.get<string>("sample-list");
+    ifstream sampleListFileReader(sampleListFile);
+    char buff[50];
+    sampleListFileReader >> buff;
+    cout << buff;
 }
 
 int main(int argc, const char **argv)
 {   
+    SampleListParser(argc, argv);
     return 0;
 }
